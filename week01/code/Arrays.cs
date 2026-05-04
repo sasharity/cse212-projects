517f7aa20fd0147f3cec2462776eadb696c19ae7
sasharity <iruogheneomaraye@gmail.com>
2026-05-04 03:20:57 +0100

    Data performance and Dynamic Arrays activities completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,19 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // My plan on how to implement Multiplesof funtion:
+        // 1. I will create a new double array of size which i will name: length
+        // 2. I will loop from 0 to length - 1
+        // 3. For each index i, i would store (number * (i + 1))
+        // 4. Then finally I will return the filled array
+        double[] result = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -26,8 +38,18 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //  My plan on how to implement the RotateListRight function
+        // 1. Find the split index: data.Count - amount
+        // 2. Store the last 'amount' elements in a temporary list
+        // 3. Remove those elements from the original list
+        // 4. Insert them at the front of the list
+
+        int splitIndex = data.Count - amount;
+
+        List<int> temp = data.GetRange(splitIndex, amount);
+
+        data.RemoveRange(splitIndex, amount);
+
+        data.InsertRange(0, temp);
     }
 }
